482f44af41cdb4cc26391f219961987c2dd2bf54
Guru Prasad Subramanian <157606881+Guru101105@users.noreply.github.com>
2025-08-15 23:39:37 +0530

    Create Day_9_Encoder4x2_design.v

---
+++ Day_9_Encoder4x2/Day_9_Encoder4x2_design.v
@@ -0,0 +1,12 @@
+module encoder4x2(
+  input [3:0]x,
+  output reg [1:0]y);
+  always@(*) begin
+    case(x)
+      4'b1000:assign y=2'b00;
+      4'b0100:assign y=2'b01;
+      4'b0010:assign y=2'b10;
+      4'b0001:assign y=2'b11;
+    endcase
+  end
+endmodule
